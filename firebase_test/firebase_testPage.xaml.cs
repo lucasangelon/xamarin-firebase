67a39693697b18695df66731728ad3d1b58a006d
Lucas Angelon <lucas.a.arouca@gmail.com>
2016-09-07 22:51:33 +0800

    feat(structure): add base structure to the project

---
+++ firebase_test/firebase_testPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace firebase_test
+{
+	public partial class firebase_testPage : ContentPage
+	{
+		public firebase_testPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
